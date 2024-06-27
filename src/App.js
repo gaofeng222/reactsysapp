b7c003accc0979a2c042a948a171a85b63bd2d34
gaofeng222 <596642721@qq.com>
2024-06-27 10:08:38 +0800

    add:受控组件

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
 import "./App.css";
 import { useState } from "react";
-import "./style.css";
-import _ from "lodash";
-import classNames from "classnames";
 
 function App() {
-  const tabs = [
-    {
-      type: "hot",
-      text: "最热",
-    },
-    {
-      type: "new",
-      text: "最新",
-    },
-  ];
-  const [nowTab, setNowTab] = useState({
-    type: "hot",
-    text: "最热",
-  });
-  const handleChangeTab = (tab) => {
-    setNowTab(tab);
-  };
-  const renderTab = (tabs) => {
-    return tabs.map((tab, index) => {
-      /* className={`nav-item ${nowTab.type === tab.type && "active"}`} */
-      return (
-        <span
-          onClick={() => handleChangeTab(tab)}
-          className={classNames(
-            "nav-item",
-            nowTab.type === tab.type && "active"
-          )}
-          key={index}
-        >
-          {tab.text} {index === tabs.length - 1 ? "" : " | "}
-        </span>
-      );
-    });
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
   return (
     <div className="App">
-      <div>{renderTab(tabs)}</div>
-      <div>
-        {nowTab.type === "hot" ? (
-          <div>
-            <h1>最热</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>最新</h1>
-          </div>
-        )}
-      </div>
+      <h2>{value}</h2>
+      用户名：
+      <input type="text" value={value} onChange={handleChange} />
     </div>
   );
 }
