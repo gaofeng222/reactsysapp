8cb52704043140435aeb310d8b8392fd4cbe552b
gaofeng222 <596642721@qq.com>
2024-06-27 10:54:40 +0800

    add:完成组件的demo实例

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import "./App.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [content, setContent] = useState("");
+  const [commentList, setCommentList] = useState([
+    {
+      id: 1,
+      content: "评论内容111",
+    },
+    {
+      id: 2,
+      content: "评论内容222",
+    },
+    {
+      id: 3,
+      content: "评论内容333",
+    },
+  ]);
+  const inputRef = useRef();
+
+  //渲染完之后才能获取生成的dom
+  const showDom = () => {
+    console.dir(inputRef.current);
+  };
+  const onSubmit = () => {
+    setCommentList([
+      ...commentList,
+      {
+        id: uuid(),
+        content,
+        ctime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      },
+    ]);
+
+    setContent("");
+    inputRef.current.focus();
+  };
   const handleChange = (e) => {
-    setValue(e.target.value);
+    console.log("🚀 ~ handleChange ~ e:", e.target.value);
+    setContent(e.target.value);
+  };
+  const renderListContent = () => {
+    return commentList.map((item) => {
+      return (
+        <li key={item}>
+          {item.content} {item.ctime ? item.ctime : "-"}
+        </li>
+      );
+    });
   };
   return (
     <div className="App">
-      <h2>{value}</h2>
-      用户名：
-      <input type="text" value={value} onChange={handleChange} />
+      <textarea
+        className="reply-box-textarea"
+        placeholder="发一条评论"
+        value={content}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <button onClick={onSubmit}>发布</button>
+      <ul>{renderListContent()}</ul>
     </div>
   );
 }
