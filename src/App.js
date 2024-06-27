4df6cbe173f539c174fcd460c950a3c9c0a1a852
gaofeng222 <596642721@qq.com>
2024-06-27 12:31:42 +0800

    add:清楚副作用

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { getUserInfo } from "./api";
 
-function App() {
-  console.log("aa");
-  const [userInfo, setUserInfo] = useState(null);
+function Demo1() {
   useEffect(() => {
-    const getUserInfoData = async () => {
-      const res = await getUserInfo();
-      console.log(res);
-      setUserInfo(res);
-    };
-    getUserInfoData();
+    const id = setInterval(() => {
+      console.log("demo1");
+    }, 1000);
     return () => {
-      console.log("cc");
+      console.log("demo1 unmount");
+      clearInterval(id);
     };
-  });
+  }, []);
+  return <h3>demo1 comp.</h3>;
+}
+function Demo2() {
+  return <h3>demo2 comp.</h3>;
+}
+
+function App() {
+  const [isShow, setIsShow] = useState(true);
+  const changeShow = () => {
+    setIsShow(!isShow);
+  };
   return (
     <div className="App">
       <h1>Hello World~~ useEffect</h1>
-      <ul>
-        <li>{userInfo?.name}</li>
-        <li>{userInfo?.age}</li>
-      </ul>
+      {isShow ? <Demo1 /> : <Demo2 />}
+      <button onClick={changeShow}>change comp.</button>
     </div>
   );
 }
