282e60020f0a59873687e156b54c84d1027c8716
gaofeng222 <596642721@qq.com>
2024-06-27 10:03:38 +0800

    add:classnames插件使用

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import "./App.css";
 import { useState } from "react";
+import "./style.css";
+import _ from "lodash";
+import classNames from "classnames";
 
 function App() {
-  let [count, setCount] = useState(0);
-  const [form, setFrom] = useState({ name: "张三", age: 18 });
-  const handleClick = () => {
-    const nums = ++count;
-    setCount(nums);
+  const tabs = [
+    {
+      type: "hot",
+      text: "最热",
+    },
+    {
+      type: "new",
+      text: "最新",
+    },
+  ];
+  const [nowTab, setNowTab] = useState({
+    type: "hot",
+    text: "最热",
+  });
+  const handleChangeTab = (tab) => {
+    setNowTab(tab);
   };
-  const handleChangeName = () => {
-    setFrom({
-      ...form,
-      name: "李四",
+  const renderTab = (tabs) => {
+    return tabs.map((tab, index) => {
+      /* className={`nav-item ${nowTab.type === tab.type && "active"}`} */
+      return (
+        <span
+          onClick={() => handleChangeTab(tab)}
+          className={classNames(
+            "nav-item",
+            nowTab.type === tab.type && "active"
+          )}
+          key={index}
+        >
+          {tab.text} {index === tabs.length - 1 ? "" : " | "}
+        </span>
+      );
     });
   };
   return (
     <div className="App">
-      <button onClick={handleClick}>{count}</button>
+      <div>{renderTab(tabs)}</div>
       <div>
-        <h2>
-          {form.name}---{form.age}
-        </h2>
-        <button onClick={handleChangeName}>change name</button>
+        {nowTab.type === "hot" ? (
+          <div>
+            <h1>最热</h1>
+          </div>
+        ) : (
+          <div>
+            <h1>最新</h1>
+          </div>
+        )}
       </div>
     </div>
   );
